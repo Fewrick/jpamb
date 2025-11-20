8d53f911a09d1b10130f6b9a19dfae9c5b15a95e
OliverWinberg <oliver@winberg.dk>
2025-11-20 17:32:50 +0100

    Syntactic analysis collects and outputs ranges for the fuzzer

--- src/main/java/jpamb/cases/Loops.java
+++ src/main/java/jpamb/cases/Loops.java
@@ -38,4 +38,58 @@ public class Loops {
     }
     assert false;
   }
+
+  @Case("(5) -> small")
+  @Case("(10) -> medium")
+  @Case("(20) -> large")
+  public static String testMultipleBranches(int x) {
+    if (x < 10) {
+      return "small";
+    }
+    if (x < 20) {
+      return "medium";
+    }
+    return "large";
+  }
+
+  @Case("(5) -> less")
+  @Case("(10) -> equal")  
+  @Case("(15) -> greater")
+  public static String testEqual(int x) {
+      if (x == 10) {
+          return "equal";
+      }
+      if (x < 10) {
+          return "less";
+      }
+      return "greater";
+  }
+
+  @Case("(5) -> not equal")
+  @Case("(10) -> is ten")
+  public static String testNotEqual(int x) {
+      if (x != 10) {
+          return "not equal";
+      }
+      return "is ten";
+  }
+  
+  @Case("(15) -> ok")
+  @Case("(5) -> too small")
+  public static String testGreaterOrEqual(int x) {
+      if (x >= 10) {
+          return "ok";
+      }
+      return "too small";
+  }
+
+  @Case("(5) -> ok")
+  @Case("(15) -> too big")
+  public static String testLessOrEqual(int x) {
+      if (x <= 10) {
+          return "ok";
+      }
+      return "too big";
+  }
+
 }
